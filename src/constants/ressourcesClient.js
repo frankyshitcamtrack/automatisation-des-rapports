7bb5d2c7c1b048a10e5a43db6b3068618a58503a
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2024-11-18 08:30:38 +0100

    ad new service

--- src/constants/ressourcesClient.js
+++ src/constants/ressourcesClient.js
@@ -1,6 +1,6 @@
-const ADMIN_ADDAX='admin ADDAX'
-const ADMIN_PERENCO='Admin PERENCO'
-const ADMIN_SABC='admin SABC'
+const ADMIN_ADDAX = "admin ADDAX";
+const ADMIN_PERENCO = "Admin PERENCO";
+const ADMIN_SABC = "admin SABC";
+const ADMIN_RAZEL = "admin razel";
 
-
-module.exports={ ADMIN_ADDAX,ADMIN_PERENCO, ADMIN_SABC }
+module.exports = { ADMIN_ADDAX, ADMIN_PERENCO, ADMIN_SABC, ADMIN_RAZEL };
